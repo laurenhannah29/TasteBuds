bc351f24b02a018d5975a84a2bba4ecbf46f8901
jomart-gsu <jmartin191@gsu.edu>
2022-02-25 17:58:04 -0500

    adding Flask app stub and package-lock

---
+++ app.py
@@ -0,0 +1,25 @@
+import flask
+
+app = flask.Flask(__name__)
+
+# set up a separate route to serve the index.html file generated
+# by create-react-app/npm run build.
+# By doing this, we make it so you can paste in all your old app routes
+# from Milestone 2 without interfering with the functionality here.
+bp = flask.Blueprint(
+    "bp",
+    __name__,
+    template_folder="./static/react",
+)
+
+# route for serving React page
+@bp.route("/")
+def index():
+    # NB: DO NOT add an "index.html" file in your normal templates folder
+    # Flask will stop serving this React page correctly
+    return flask.render_template("index.html")
+
+
+app.register_blueprint(bp)
+
+app.run()
