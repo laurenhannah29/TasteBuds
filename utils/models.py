241fa3f8d8b5a0d579c021b8bf2896ba5fb99b32
Sage <spapbp045@gmail.com>
2022-04-02 18:54:29 -0400

    added docstring to models

--- utils/models.py
+++ utils/models.py
@@ -16,20 +16,40 @@ class User(UserMixin, db.Model):
 
 
 class Posts(db.Model):
+    """
+    id(Intger) primary key
+    user_id(Integer) the creater of the post
+    image(String) I am assuming this a url
+    caption(String) caption under the post
+    """
+
     id = db.Column(db.Integer, primary_key=True)
-    creater = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
     image = db.Column(db.String())
     caption = db.Column(db.String())
 
 
 class Comments(db.Model):
+    """
+    id(Integer) primary key
+    user_id(Intger) user that creater comment
+    post_id(Integer) post that this under
+    comment(String) the content
+    """
+
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.String())
+    user_id = db.Column(db.Integer())
     post_id = db.Column(db.Integer)
     comment = db.Column(db.String())
 
 
 class Saved(db.Model):
+    """
+    id(Integer) primary key
+    user_id(Intger) user that saved
+    post_id(Integer) post that is saved
+    """
+
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
     post_id = db.Column(db.Integer)
