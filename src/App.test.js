273072a9676c8942b8b7a8bf24648d6083307379
cw87241 <chyla.walker@hotmail.com>
2022-04-08 19:39:14 -0400

    added two front end tests for profile page

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import EditProfile from './EditProfile';
+import Profile from './Profile';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders header sentence', () => {
+  render(<EditProfile />);
+  const linkElement = screen.getByText(/Edit profile/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders save button being on page', () => {
+  render(<EditProfile />);
+  const linkElement = screen.getByText(/Save/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
