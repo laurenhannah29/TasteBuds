14be352037c9f158f77214646df629265eef0c74
sage <spapbp045@gmail.com>
2022-04-20 08:34:51 -0400

    commented some links for pres

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,10 @@ const Navbar = () => {
         <div class="navabr">
             <li><Link to="/home">Home</Link></li>
             <li><a href="/profile">Profile</a></li>
-            <li><a href="/Nutrition">Nutrition</a></li>
+            {/* <li><a href="/Nutrition">Nutrition</a></li> */}
             <li><Link to="/saved">Saved</Link></li>
             <li><Link to="/NewPost">Create a Post!!!</Link></li>
-            <li><Link to="/Appliance">See what I just added</Link></li>
+            {/* <li><Link to="/Appliance">See what I just added</Link></li> */}
             <li><a href="/login">Login</a></li>
             <li><a href="/logout">Logout</a></li>
         </div>
