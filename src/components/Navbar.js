766d2394f175b382c41dd005125f754430705be8
Lauren <laurenhannah@Laurens-MacBook-Air.local>
2022-04-19 23:26:26 -0400

    nutrition commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,10 @@ const Navbar = () => {
         <div class="navabr">
             <li><Link to="/">Home     </Link></li>
             <li><a href="/profile">Profile     </a></li>
-            <li><a href="/Nutrition">Nutrition     </a></li>
-            <li><Link to="/saved">Saved     </Link></li>
-            {/* Maybe some logic to determine what is in here such logout or login */}
             <li><Link to="/NewPost">Create a Post!!!     </Link></li>
+            <li><Link to="/saved">Saved     </Link></li>
+            <li><a href="/Nutrition">Nutrition     </a></li>
+            {/* Maybe some logic to determine what is in here such logout or login */}
             <li><a href="/login">Login    </a></li>
             <li><a href="/logout">Logout</a></li>
         </div>
